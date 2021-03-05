1b903a46237136ebceae987acfb1cdfa153739ee
Umair Syed <naqvi.umair@gmail.com>
2021-03-05 15:35:52 +0100

    project setup

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# DailyLifeHelper
+Daily Life Helper is developed with Blazor Mobile Bindings. its a Hybrid Mobile App using C# and .NET for Android, iOS, Windows, macOS
---
+++ Src/DailyLifeHelper.Windows/App.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Microsoft.MobileBlazorBindings.WebView.Windows;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WPF;
+
+namespace DailyLifeHelper.Windows
+{
+    public class MainWindow : FormsApplicationPage
+    {
+        [STAThread]
+        public static void Main()
+        {
+            var app = new System.Windows.Application();
+            app.Run(new MainWindow());
+        }
+
+        public MainWindow()
+        {
+            Forms.Init();
+            BlazorHybridWindows.Init();
+            LoadApplication(new App());
+        }
+    }
+}
